541fcbcd1cdeabfdb7dd24776d3ae9e32bc53254
Karate_Private <karatemohammed4@gmail.com>
2021-11-19 20:26:54 +0200

    just a test on windows for git 1

--- Loop_Scripts.py
+++ Loop_Scripts.py
@@ -15,14 +15,16 @@ print("==============================================\n\n")
 # 	print (f"sleep {i+1} \n")
 # 	time.sleep(5)
 
-print ("\n\n\n Ping Test is finishined let's take a break and Continue to Automation Script ^_^ \n\n\n\n")
-time.sleep(6)
-for i in range (5) :
-	print(f"START  {i+1}\n")
-	print ("########################################################################")
-	os.system('python3 /home/khayat/Automation/Discover_New_IPs.py')
-	print(f"END  {i+1}")
-	print ("########################################################################")
-	print (f"sleep {i+1} \n")
-	time.sleep(5)
+
+# print ("\n\n\n Ping Test is finishined let's take a break and Continue to Automation Script ^_^ \n\n\n\n")
+# time.sleep(6)
+# for i in range (5) :
+# 	print(f"START  {i+1}\n")
+# 	print ("########################################################################")
+# 	os.system('python3 /home/khayat/Automation/Discover_New_IPs.py')
+# 	print(f"END  {i+1}")
+# 	print ("########################################################################")
+# 	print (f"sleep {i+1} \n")
+# 	time.sleep(5)
+
 
